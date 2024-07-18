e5583f143471d57a5f85f1eee872cad8b67c1a19
Dean Divizio <dean@deandivizio.com>
2024-07-18 09:11:17 -0400

    Initial Commit

---
+++ Data Deck/Data_DeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_DeckApp.swift
+//  Data Deck
+//
+//  Created by Dean Divizio on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Data_DeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
